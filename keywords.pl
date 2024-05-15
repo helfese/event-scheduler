2a564349f769a9537f9f234652ee0bef1eb92468
Hélio Fernandes Sebastião <155336173+helfese@users.noreply.github.com>
2024-05-15 12:26:05 +0100

    Create keywords.pl

---
+++ keywords.pl
@@ -0,0 +1,13 @@
+rooms(lectureHallsLarge, ['a1', 'a2']).
+rooms(LectureHallsSmall, ['a3', 'a4', 'a5']).
+rooms(classrooms, ['0-9', '0-13', '0-15', '0-16', '0-17', '0-23', '0-25', '0-49', '0-71', '0-73', '0-75', '1-2', '1-3', '1-4', '1-11', '1-22', '1-24']).
+rooms(labsPC, ['0-14', '0-21', '0-27', '1-15', '1-17', '1-19', '1-27', '1-29']).
+rooms(labsElectro, ['1-60', '1-62', '1-64']).
+rooms(labsChem, ['1-75']).
+rooms(labsPhysics, ['1-12', '1-14']).
+rooms(labsNetwork, ['1-26', '1-28', '1-30']).
+rooms(labsGame, ['1-32']).
+rooms(videoConf, ['0-19', '0-13']).
+
+bachelors(tagus, ['lee', 'legi', 'leic-t', 'leti']).
+masters(tagus, ['mbmrp21', 'mee', 'megi', 'meic-t', 'meti']).
